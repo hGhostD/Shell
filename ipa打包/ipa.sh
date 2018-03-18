f7e7a2423cebff4403268564704a9b1df814f975
644727408@qq.com <644727408@qq.com>
2018-03-18 22:41:04 +0800

    fastlane Fir 上传 脚本

---
+++ ipa打包/ipa.sh
@@ -0,0 +1,47 @@
+Fir_token="554c539d32252425f397dbf39e1831d9"
+# 参数传递目标工程 和 版本号
+rootPath=$1
+version=$2
+# 工程名默认与文件夹同名
+project_name=$(basename $rootPath)
+# scheme 默认与工程名同名
+scheme_name=$project_name
+# Info.plist 文件路径
+infoPath=$project_name/Info.plist
+# 打包时间 设置为 build 号码
+BuildDate=$(date +%Y%m%d%H%M)
+
+# 指定项目地址
+workspace_path="$rootPath/$scheme_name.xcworkspace"
+# 指定输出路径
+output_path=$(pwd)/build
+# 指定输出归档文件地址
+archive_path="$output_path/archive/${scheme_name}_${BuildDate}.xcarchive"
+# 指定输出ipa地址
+ipa_path="$output_path/$scheme_name"
+# 指定输出ipa名称
+ipa_name="${scheme_name}_${BuildDate}.ipa"
+# 指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
+export_method="app-store"
+
+
+echo "============================ 正在 修改Build ============================"
+cd $rootPath
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" $infoPath || exit
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BuildDate" $infoPath || exit
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!! 修改 完成 ${version} ${BuildDate}   !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+echo "============================ 开始打包 ipa 文件 ============================"
+echo ${scheme_name}
+fastlane fir scheme:"${scheme_name}" \
+			 buildDate:"${BuildDate}" \
+			 archive_path:"${archive_path}" \
+			 outPath:"${ipa_path}" \
+			 ipa_name:"${ipa_name}"
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!! 打包完成 !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+#上传到fir
+echo "============================ 开始上传 ipa 文件 ============================"
+fir publish "${ipa_path}/${ipa_name}" -T "${Fir_token}" -c
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!! 上传完成 !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
